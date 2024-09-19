3e02715ad26c873bdff68a398d0122b27cb90ac1
lancepr1 <108348802+lancepr1@users.noreply.github.com>
2024-09-19 09:00:04 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,9 @@
+public class Main{
+   public static void main(String[] args) {
+       Feeder f = new Feeder();
+       System.out.println(f.getCurrentFood());
+       Feeder g = new Feeder(500);
+       System.out.println(g.getCurrentFood());
+   }
+}
 
